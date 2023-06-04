86e9dad30c62dc9b307963d2bccbc8657a4559d9
Brian Nguyen <briannguyen980@gmail.com>
2023-06-04 16:32:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import random
 pygame.init()
 
 # constants
-height = 620
+height = 601
 width = 500
 title = "tetris"
 game_height = 600
@@ -36,6 +36,103 @@ pygame.display.set_caption(title)
 
  # colors ( red, green, blue)
 
+
+running = True
+
+
+
+# def fill_board(pos={}):
+#     board = [[0,0,0] for _ in  range(10) for _ in range(20)]
+#     for i in range(board):
+#         for j in range(len(board[i])):
+#             if(j,i) in pos:
+#                 temp = pos[(j,i)]
+#                 board[i][j] = temp
+#     return board
+
+        
+
+l = [['0...',
+      '0...',
+      '00..',
+      '....'],
+     ['....',
+      '000.',
+      '0...',
+      '....'],
+     ['00..',
+      '.0..',
+      '.0..',
+      '....'],
+     ['..0.',
+      '000.',
+      '....',
+      '....']]  
+
+i = [['0...',
+      '0...',
+      '0...',
+      '0...'],
+     ['....',
+      '0000',
+      '....',
+      '....']]
+
+r = [['00..',
+      '0...',
+      '0...',
+      '....'],
+     ['0...',
+      '000.',
+      '....',
+      '....'],
+     ['.0..',
+      '.0..',
+      '00..',
+      '....'],
+     ['....',
+      '000.',
+      '..0.',
+      '....']]
+
+o = [['00..',
+      '00..',
+      '....',
+      '....']]
+
+s = [['.00.',
+      '00..',
+      '....',
+      '....'],
+     ['0...',
+      '00..',
+      '.0..',
+      '....']]
+
+t = [['000.',
+      '.0..',
+      '....',
+      '....'],
+     ['.0.',
+      '00..',
+      '.0..',
+      '....'],
+     ['.0..',
+      '000.',
+      '....',
+      '....'],
+     ['.0..',
+      '.00.',
+      '.0..',
+      '....']]
+z = [['00..',
+      '.00.',
+      '....',
+      '....'],
+     ['.0..',
+      '00..',
+      '0...',
+      '....']]
 white = (255, 255, 255)
 black = (0, 0, 0)
 cyan = (0,255, 255)
@@ -46,51 +143,97 @@ green = (0,255, 0)
 purple = (160,32, 240)
 red = (255,0, 0)
 
-running = True
+shapes = [l,i,r,o,s,t,z]
+colors = [cyan, blue,orange,yellow, green,purple,red]
 
-
-
-def fill_board(pos={}):
-    board = [[0,0,0] for _ in  range(10) for _ in range(20)]
-    for i in range(board):
-        for j in range(len(board[i])):
-            if(j,i) in pos:
-                temp = pos[(j,i)]
-                board[i][j] = temp
-    return board
-
-def draw_grid():
+class Piece(object):
+    def __init__(self, x, y, shape):
+        self.x = x
+        self.y = y
+        self.shape = shape
+        self.color = colors[shapes.index(shape)]
+        self.rotation = 0
+        
+def draw_window(screen, grid, score = 0, lastscore = 0):
+        screen.fill(black)
+        pygame.font.init()
+        font = pygame.font.SysFont('Arial',60)
+        label = font.render("Tetris", 1, white)
+        draw_grid(screen, grid)
+        
+        
+def draw_grid(screen, grid):
     x = game_width // cell + 1
     y = game_height // cell + 1
-    for i in range(x):
+    for i in range(len(grid)):
         pygame.draw.line(screen, white, (i * cell, 0), (i * cell, game_height))
-    for j in range(y):
-        pygame.draw.line(screen, white, (0, j * cell), (game_width, j * cell))
+        for j in range(len(grid[i])):
+            pygame.draw.line(screen, white, (0, j * cell), (game_width, j * cell))
+
+def create_grid(locked_pos={}):
+    grid = [[(0,0,0) for i in range(10)] for j in range(20)]
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if(j,i) in locked_pos:
+                c = locked_pos[(j,i)]
+                grid[i][j] = c
+    return grid
+
+def if_valid(shape, grid):
+    vaild_pos = [[(j,i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
+    vaild_pos = [j for sub in vaild_pos for j in sub]
+    
+    format = convert_shapes(shape)
+    
+    for pos in format:
+        if pos not in vaild_pos:
+            if pos[1]> -1:
+                return False
+    return True;   
+ 
+def convert_shapes(shape):
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+    
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, col in enumerate(row):
+            if col == '0':
+                positions.append((shape.x + j, shape.y + i))
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0], pos[1])   
+    return positions
+        
+
+def get_shape():
+    return Piece(5, 0, random.choice(shapes))      
         
         
-shapes = [
-    [(1,0), (1,1), (1,2),(0,2)], # l shape
-    [(0,0), (0,1), (0,2),(0,3)], # i shape
-    [(0,0), (1,0), (1,1),(1,2)], # r shape
-    [(0,0), (0,1), (1,0),(1,1)], # o shape
-    [(1,0), (1,1), (0,1),(0,2)], # s shape
-    [(1,0), (1,1), (1,2),(0,1)], # t shape
-    [(0,0), (0,1), (1,1),(1,2)], # z shape
-]
-
-colors= [white, black,cyan, blue,orange,yellow, green,purple,red]
-
-
-while(running):
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            running = False
-            
-    screen.fill(black)
-    draw_grid()
-    pygame.display.update()
+def main():
+    screen = pygame.display.set_mode((width, height))
+    locked_positions = {}
+    grid = create_grid(locked_positions)
+    current = get_shape()
+    change = False
+    next = get_shape()
+    fall_speed = 0.5
+    score = 0
+    while(True):
+        grid = create_grid(locked_positions)
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                running = False
         
-pygame.quit()
+        
+        current.y+=1
+        if not(if_valid(current, grid)) and current.y > 0:
+            current.y -= 1
+            change = True
+        draw_window(screen, grid,score,score)     
+        pygame.display.update()
+        
+main()
         
 
 
